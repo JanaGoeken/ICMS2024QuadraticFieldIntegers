eb36fb34f2bc59c4e4348279133a0d3b1eb8aea7
JanaGoeken <goeken@uni-bremen.de>
2024-05-27 18:08:54 +0200

    Step 1 complete

--- Jana_ICMS2024QuadraticFieldIntegers.lean
+++ Jana_ICMS2024QuadraticFieldIntegers.lean
@@ -1,3 +1,14 @@
 import Mathlib.NumberTheory.NumberField.Basic
+import Mathlib.FieldTheory.PrimitiveElement
 
-theorem PrimitiveElementsOfDImTwo {K : Type*}[Field K][Algebra ℚ K](h : findim ℚ K = 2) : ∃ (α : K), K ≅ Q⟮α⟯
+open FiniteDimensional Polynomial IntermediateField
+
+theorem PrimitiveElementsOfDImTwo {K : Type*}[Field K][Algebra ℚ K](h : finrank ℚ K = 2) :
+    ∃ (α : K),  ∃ φ: K ≃+* ℚ⟮α⟯, true := by
+  have : FiniteDimensional ℚ K := by exact Module.finite_of_finrank_eq_succ h
+  obtain ⟨α, hα⟩ := Field.exists_primitive_element ℚ K
+  use α
+  refine ⟨?_, rfl⟩
+  let φ := (IntermediateField.topEquiv (F := ℚ) (E := K)).symm
+  rw [hα]
+  apply φ.toRingEquiv
