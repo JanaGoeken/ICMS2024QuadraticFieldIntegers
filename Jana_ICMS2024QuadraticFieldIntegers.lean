21267ea0981dd4f280081b6301bca191f46a1fe5
JanaGoeken <goeken@uni-bremen.de>
2024-05-28 19:29:07 +0200

    Step 3 in progress

--- Jana_ICMS2024QuadraticFieldIntegers.lean
+++ Jana_ICMS2024QuadraticFieldIntegers.lean
@@ -14,10 +14,23 @@ theorem PrimitiveElementsOfDImTwo {K : Type*}[Field K][Algebra ℚ K](h : finran
   rw [hα]
   apply φ.toRingEquiv
 
-#check (2 : ℝ)
 theorem MinpolyDegreeAtMostTwo {K : Type u_3} [Field K] [Algebra ℚ K] (α : K) (h : finrank ℚ K = 2)
 [FiniteDimensional ℚ K] : (minpoly ℚ α ).degree ≤ 2 := by
   convert minpoly.degree_le α
   · rw [h]
     norm_cast
   · assumption
+
+theorem Ednawashere {K : Type*}[Field K][Algebra ℚ K](h : finrank ℚ K = 2) :
+    ∃ (α : K) (p q s : ℚ) (r : K), α = p + q * r ∧ r^2 = s := by
+    choose α φ using PrimitiveElementsOfDImTwo h
+    have : FiniteDimensional ℚ K := by exact Module.finite_of_finrank_eq_succ h
+    have h_deg : (minpoly ℚ α ).degree ≤ 2 := by apply MinpolyDegreeAtMostTwo α h
+    have h_poly : ∃ (a b c : ℚ), a * α^2 + b * α + c = 0 := by sorry
+    choose a b c using h_poly
+    let s := b^2 - 4
+    have h_roots : α = (-b + r) / (2 * a) ∨ α = (-b -r) / (2 * a) := by sorry
+    use α
+    let p := -b / (2 * a)
+    --let q := +- 1/(2 * a)
+    sorry
